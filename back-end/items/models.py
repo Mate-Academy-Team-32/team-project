0d91cb6f3eb284eaa03819f19423e7de06841201
Davydova Olha <o.chookity@gmail.com>
2024-01-25 15:31:56 +0200

    Update Review model

--- back-end/items/models.py
+++ back-end/items/models.py
@@ -34,16 +34,6 @@ class Tag(CoreModel):
         return self.name
 
 
-class Review(CoreModel):
-    rate = models.PositiveIntegerField(
-        validators=[MinValueValidator(0), MaxValueValidator(5)]
-    )
-    text = models.TextField()
-
-    def __str__(self):
-        return f"{self.rate}/5"
-
-
 class Item(CoreModel):
     class Gender(models.TextChoices):
         FEMALE = "F", "Female"
@@ -68,12 +58,20 @@ class Item(CoreModel):
     tags = models.ManyToManyField(Tag, related_name="items", blank=True)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     inventory = models.PositiveIntegerField()
-    reviews = models.ForeignKey(
-        Review, on_delete=models.CASCADE, related_name="items", blank=True, null=True
-    )
 
     def __str__(self):
         return f"{self.id}-{self.name}"
+
+
+class Review(CoreModel):
+    item = models.ForeignKey(Item, on_delete=models.CASCADE, related_name="reviews")
+    rate = models.PositiveIntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(5)]
+    )
+    text = models.TextField()
+
+    def __str__(self):
+        return f"{self.rate}/5"
 
 
 class ItemImage(CoreModel):
