f88d78428e83899ee0b1157e4488e0995c973e8a
Davydova Olha <o.chookity@gmail.com>
2024-01-26 22:24:06 +0200

    Implement IsAdminOrReadOnly permission

--- back-end/PerfuMe_API/settings.py
+++ back-end/PerfuMe_API/settings.py
@@ -133,7 +133,8 @@ AUTH_USER_MODEL = "users.User"
 REST_FRAMEWORK = {
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
-    )
+    ),
+    "DEFAULT_PERMISSION_CLASSES": ("api.permissions.IsAdminOrReadOnly",),
 }
 
 SIMPLE_JWT = {
