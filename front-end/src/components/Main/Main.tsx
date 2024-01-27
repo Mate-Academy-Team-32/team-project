6ad8818742668da885bf7a6ff8f6d3b053a02fe7
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-27 22:28:38 +0200

    Added `Front-End` part

--- front-end/src/components/Main/Main.tsx
+++ front-end/src/components/Main/Main.tsx
@@ -1,43 +1,64 @@
+import React from 'react';
 import './Main.scss';
-import logoHeart from '../../img/logo-heart.svg';
-import logoBag from '../../img/logo-bag.svg';
 import { Slider } from '../Slider';
+import { Card } from '../Card';
+import logoPartner1 from '../../img/logo-partners-dg.svg';
+import logoPartner2 from '../../img/logo-partners-ga.svg';
+import logoPartner3 from '../../img/logo-partners-givenchy.svg';
+import logoPartner4 from '../../img/logo-partners-pcd.svg';
+import logoPartner5 from '../../img/logo-partners-sl.svg';
+import logoPartner6 from '../../img/logo-partners-chanel.svg';
 
 export const Main: React.FC = () => (
   <main className="Main">
+    <Slider
+      images={[
+        {
+          path: './img/image-slide-1.png',
+          id: 1,
+        },
+        {
+          path: './img/image-slide-2.png',
+          id: 2,
+        },
+        {
+          path: './img/image-slide-3.png',
+          id: 3,
+        },
+        {
+          path: './img/image-slide-4.png',
+          id: 4,
+        },
+        {
+          path: './img/image-slide-5.png',
+          id: 5,
+        },
+      ]}
+      timeUpdate={3}
+    />
+
+    <section className="Main__partners">
+      <a href="https://www.dolcegabbana.com/en-it/" target="_blank" rel="noopener noreferrer">
+        <img src={logoPartner1} alt="1" />
+      </a>
+      <a href="https://www.armani.com/en-wx" target="_blank" rel="noopener noreferrer">
+        <img src={logoPartner2} alt="2" />
+      </a>
+      <a href="https://www.givenchy.com/" target="_blank" rel="noopener noreferrer">
+        <img src={logoPartner3} alt="3" />
+      </a>
+      <a href="https://parfums.ua/ua/product/christine-darvin-charming-collector-edition-1" target="_blank" rel="noopener noreferrer">
+        <img src={logoPartner4} alt="4" />
+      </a>
+      <a href="https://www.ysl.com/en-en" target="_blank" rel="noopener noreferrer">
+        <img src={logoPartner5} alt="5" />
+      </a>
+      <a href="https://www.chanel.com/" target="_blank" rel="noopener noreferrer">
+        <img src={logoPartner6} alt="6" />
+      </a>
+    </section>
+
     <section className="Main__catalog">
-      <div className="Main__panel">
-        <a href="/" className="Main__logo">PerfuMe</a>
-
-        <div className="input--search">
-          <input
-            className='input'
-            type="text"
-            placeholder='Hey, what are you looking for?'
-          />
-        </div>
-
-        <a href="/" className='Main__language-select'>
-          Eng
-        </a>
-
-        <a href="/">
-          <img
-            className='Main__icon'
-            src={logoHeart}
-            alt="Heart"
-          />
-        </a>
-
-        <a href="/">
-          <img
-            className='Main__icon Main__icon--w-34'
-            src={logoBag}
-            alt="Bag"
-          />
-        </a>
-      </div>
-
       <nav className="Main__categories">
         <a href="/" className="Main__link">Women's perfumery</a>
         <a href="/" className="Main__link">Men's perfumery</a>
@@ -47,7 +68,17 @@ export const Main: React.FC = () => (
         <a href="/" className="Main__link">Gift ideas</a>
       </nav>
     </section>
+    
+    <hr className="Main__line" />
 
-    <Slider />
+    <section className="Main__store">
+      <aside className="Main__filters">
+        
+      </aside>
+
+      <div className="Main__store-browse">
+        {Array.from(Array(6)).map((_el, i) => <Card key={i} />)}
+      </div>
+    </section>
   </main>
 );
--- front-end/src/components/Slider/Slider.tsx
+++ front-end/src/components/Slider/Slider.tsx
@@ -1,11 +1,111 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './Slider.scss';
 
-export const Slider: React.FC = () => (
-  <section className='Slider'>
-    <div className="Slider__controls">
-      <div className="Slider__button Slider__button--left"></div>
-      <div className="Slider__button"></div>
-    </div>
-  </section>
-);
+type Image = {
+  path: string;
+  id: number;
+};
+
+type Props = {
+  images: Image[];
+  timeUpdate?: number;
+};
+
+export const Slider: React.FC<Props> = ({ images, timeUpdate = 1 }) => {
+  const [currentSlide, setCurrentSlide] = useState(1);
+  const countSlides = images.length;
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCurrentSlide(slide => {
+        if (slide >= countSlides) {
+          changeSelectedPage(0);
+          return 1;
+        } else {
+          changeSelectedPage(slide);
+          return slide + 1;
+        }
+      });
+    }, timeUpdate * 1000);
+
+    return () => clearInterval(interval);
+  });
+
+  const handleClickBack = () => {
+    setCurrentSlide(slide => slide > 1 ? slide - 1 : countSlides);
+    changeSelectedPage(currentSlide);
+  };
+
+  const handleClickNext = () => {
+    setCurrentSlide(slide => slide < countSlides ? slide + 1 : 1);
+    changeSelectedPage(currentSlide);
+  };
+
+  const changeSelectedPage = (index: number) => {
+    const pages = document.querySelectorAll('.page') as NodeListOf<HTMLButtonElement>;
+
+    for (let i = 0; i < countSlides; i++) {
+      pages[i].classList.remove('page--selected');
+    }
+
+    pages[index].classList.add('page--selected');
+  };
+  
+  return (
+    <section className='Slider'>
+      <div className="Slider__content">
+        <button className="Slider__button Slider__button--left" onClick={handleClickBack}></button>
+
+        <div className="Slider__description">
+          <h1 className="Slider__title">
+            Welcome to the wonderful world of perfumery PerfuMe!
+          </h1>
+          
+          <p className="Slider__text">
+            Our online store specializes in selling original perfumes. We offer a large selection of fragrances of popular and well-known brands, the quality of which is beyond doubt. We work exclusively with verified distributors and verified partners. Our prices will pleasantly surprise you!
+          </p>
+        </div>
+
+        <div className="Slider__image">
+          {images.map((image, index) => {
+            if (currentSlide === index + 1) {
+              return <img
+                src={image.path}
+                alt={(index + 1).toString()}
+                key={image.id}
+              />;
+            }
+
+            return '';
+          })}
+        </div>
+
+        <button className="Slider__button" onClick={handleClickNext}></button>
+      </div>
+
+      <div className="Slider__pages">
+        {images.map((image, index) => (
+          index === 0 ? (
+            <button
+              key={image.id}
+              className="page page--selected"
+              onClick={() => {
+                setCurrentSlide(image.id);
+              }}
+            >
+            </button>
+          ) : (
+            <button
+              key={image.id}
+              className="page"
+              onClick={() => {
+                setCurrentSlide(image.id);
+              }}
+            >
+            </button>
+          )
+        ))}
+      </div>
+    </section>
+  );
+};
