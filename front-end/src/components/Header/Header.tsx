9fbad55ecb0854b9f47f8b36b21d5748d1e45af5
Yehor Korablin <yegor_ka2004@ukr.net>
2024-01-29 19:13:32 +0200

    Fixed margin at Header

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -16,62 +16,65 @@ window.addEventListener('scroll', () => {
 });
 
 export const Header: React.FC = () => (
-  <header className="Header">
-    <section className="Header__top-bar">
-      <nav className="nav nav--links">
-        <a href="/" className="nav__link">Home</a>
-        <a href="/" className="nav__link">About us</a>
-        <a href="/" className="nav__link">Delivery and payment</a>
-        <a href="/" className="nav__link">Blog</a>
-        <a href="/" className="nav__link">Contacts</a>
-      </nav>
-      <nav className="nav nav--sign">
-        <a href="/sign">
-          <img
-            className="sign-in"
-            src={logoAccount}
-            alt="Sign in"
-          />
-        </a>
+  <>
+    <header className="Header">
+      <section className="Header__top-bar">
+        <nav className="nav nav--links">
+          <a href="/" className="nav__link">Home</a>
+          <a href="/" className="nav__link">About us</a>
+          <a href="/" className="nav__link">Delivery and payment</a>
+          <a href="/" className="nav__link">Blog</a>
+          <a href="/" className="nav__link">Contacts</a>
+        </nav>
+        <nav className="nav nav--sign">
+          <a href="/sign">
+            <img
+              className="sign-in"
+              src={logoAccount}
+              alt="Sign in"
+            />
+          </a>
 
-        <a href="/" className='Header__language-select'>
-          Eng
-        </a>
-      </nav>
-    </section>
+          <a href="/" className='Header__language-select'>
+            Eng
+          </a>
+        </nav>
+      </section>
 
-    <section className="Header__panel">
-      <div className="Header__left-side">
-        <a href="/" className="Header__logo">
-          <img src={logoPerfume} alt="Logo PerfuMe" />
-        </a>
-      </div>
-
-      <div className="Header__right-side">
-        <div className="input--search">
-          <input
-            className='input'
-            type="text"
-            placeholder='Hey, what are you looking for?'
-          />
+      <section className="Header__panel">
+        <div className="Header__left-side">
+          <a href="/" className="Header__logo">
+            <img src={logoPerfume} alt="Logo PerfuMe" />
+          </a>
         </div>
 
-        <a href="/">
-          <img
-            className='Header__icon'
-            src={logoHeart}
-            alt="Heart"
-          />
-        </a>
+        <div className="Header__right-side">
+          <div className="input--search">
+            <input
+              className='input'
+              type="text"
+              placeholder='Hey, what are you looking for?'
+            />
+          </div>
 
-        <a href="/">
-          <img
-            className='Header__icon Header__icon--w-34'
-            src={logoBag}
-            alt="Bag"
-          />
-        </a>
-      </div>
-    </section>
-  </header>
+          <a href="/">
+            <img
+              className='Header__icon'
+              src={logoHeart}
+              alt="Heart"
+            />
+          </a>
+
+          <a href="/">
+            <img
+              className='Header__icon Header__icon--w-34'
+              src={logoBag}
+              alt="Bag"
+            />
+          </a>
+        </div>
+      </section>
+    </header>
+    <div className="margin"></div>
+  </>
 );
