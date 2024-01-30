0bfc9585f06a94a49e8697341e6642b6c710c55b
Yehor Korablin <yehorkorablin@gmail.com>
2024-01-30 23:41:41 +0200

    Replaced partners logo

--- src/components/Main/Main.tsx
+++ src/components/Main/Main.tsx
@@ -3,12 +3,11 @@ import './Main.scss';
 import { Slider } from '../Slider';
 import { Card } from '../Card';
 import { TwoThumbInputRange } from 'react-two-thumb-input-range';
-import logoPartner1 from '../../img/logo-partners-dg.svg';
-import logoPartner2 from '../../img/logo-partners-ga.svg';
-import logoPartner3 from '../../img/logo-partners-givenchy.svg';
-import logoPartner4 from '../../img/logo-partners-pcd.svg';
-import logoPartner5 from '../../img/logo-partners-sl.svg';
-import logoPartner6 from '../../img/logo-partners-chanel.svg';
+import logoPartner1 from '../../img/logo-partners-1.svg';
+import logoPartner2 from '../../img/logo-partners-2.svg';
+import logoPartner3 from '../../img/logo-partners-3.svg';
+import logoPartner4 from '../../img/logo-partners-4.svg';
+import logoPartner5 from '../../img/logo-partners-5.svg';
 import imgOffer from '../../img/image-offer.png';
 import imgChanelWoman from '../../img/image-chanel-woman.png';
 import imgChanelBottle from '../../img/image-chanel-bottle.png';
@@ -68,32 +67,18 @@ export const Main: React.FC = () => {
         <a href="https://www.dolcegabbana.com/en-it/" target="_blank" rel="noopener noreferrer">
           <img src={logoPartner1} alt="1" />
         </a>
-        <a href="https://www.armani.com/en-wx" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.gucci.com/int/en/" target="_blank" rel="noopener noreferrer">
           <img src={logoPartner2} alt="2" />
         </a>
-        <a href="https://www.givenchy.com/" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.armani.com/en-wx" target="_blank" rel="noopener noreferrer">
           <img src={logoPartner3} alt="3" />
         </a>
-        <a href="https://parfums.ua/ua/product/christine-darvin-charming-collector-edition-1" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.givenchy.com/" target="_blank" rel="noopener noreferrer">
           <img src={logoPartner4} alt="4" />
         </a>
-        <a href="https://www.ysl.com/en-en" target="_blank" rel="noopener noreferrer">
+        <a href="https://www.chanel.com/gb/" target="_blank" rel="noopener noreferrer">
           <img src={logoPartner5} alt="5" />
         </a>
-        <a href="https://www.chanel.com/" target="_blank" rel="noopener noreferrer">
-          <img src={logoPartner6} alt="6" />
-        </a>
-      </section>
-
-      <section className="Main__catalog">
-        <nav className="Main__categories">
-          <a href="/" className="Main__link">Women's perfumery</a>
-          <a href="/" className="Main__link">Men's perfumery</a>
-          <a href="/" className="Main__link">Children's perfumery</a>
-          <a href="/" className="Main__link">New</a>
-          <a href="/" className="Main__link">Actions</a>
-          <a href="/" className="Main__link">Gift ideas</a>
-        </nav>
       </section>
     
       <hr className="Main__line" />
