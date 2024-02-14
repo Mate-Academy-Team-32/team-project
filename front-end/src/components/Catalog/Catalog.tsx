875cc704fa6dd5cf2be9cd7f6affcc4fcef4182d
Yehor Korablin <yehorkorablin@gmail.com>
2024-02-14 08:11:21 +0200

    Added new dropdown menu

--- front-end/src/components/Catalog/Catalog.tsx
+++ front-end/src/components/Catalog/Catalog.tsx
@@ -95,6 +95,7 @@ export const Catalog: React.FC = () => {
 
       <section className="Catalog__pre">
         <h1 className="Catalog__head">Top 10 fragrances of the season</h1>
+        {/* That dropdown written on Tailwind will be deleted */}
         <div className="Catalog__buttons">
           <section
             className={
@@ -150,6 +151,32 @@ export const Catalog: React.FC = () => {
           >
             {view} View
           </button>
+        </div>
+
+        <div className="dropdown is-active">
+          <div className="dropdown-trigger">
+            <button className="button" aria-haspopup="true" aria-controls="dropdown-menu2">
+              <span>Content</span>
+              <span className="icon is-small">
+                <i className="fas fa-angle-down" aria-hidden="true"></i>
+              </span>
+            </button>
+          </div>
+          <div className="dropdown-menu" id="dropdown-menu2" role="menu">
+            <div className="dropdown-content">
+              <div className="dropdown-item">
+                <p>You can insert <strong>any type of content</strong> within the dropdown menu.</p>
+              </div>
+              <hr className="dropdown-divider" />
+                <div className="dropdown-item">
+                  <p>You simply need to use a <code>&lt;div&gt;</code> instead.</p>
+                </div>
+                <hr className="dropdown-divider" />
+                  <a href="#" className="dropdown-item">
+                    This is a link
+                  </a>
+            </div>
+          </div>
         </div>
       </section>
 
