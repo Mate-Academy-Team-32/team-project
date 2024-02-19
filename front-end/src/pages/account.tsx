1e17f98ec3c595a23dc8bd417ce54827d36e4e05
Yehor Korablin <yehorkorablin@gmail.com>
2024-02-19 17:22:52 +0200

    Fixed Account page

--- front-end/src/pages/account.tsx
+++ front-end/src/pages/account.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { Account as Verification } from '../components/Account';
-import { Header } from '../components/Header';
-import { Footer } from '../components/Footer';
 
 export const Account: React.FC = () => (
-  <>
-    <Header />
-    <Verification />
-    <Footer />
-  </>
+  <Verification />
 );
