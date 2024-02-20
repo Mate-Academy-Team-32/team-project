e0f7c474e3ebe571bedaa0922a431f4d365d19d1
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2024-02-20 01:59:08 -0600

    Add cart_item and favorite_item endpoints

--- back-end/PerfuMe_API/settings.py
+++ back-end/PerfuMe_API/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = "django-insecure-9@#+2+d)^(us(jc%=dlw1v8y*&g$co%8517s&i+a&tz@k62s_l
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["0.0.0.0", "localhost"]
+ALLOWED_HOSTS = ["0.0.0.0", "localhost", "127.0.0.1"]
 
 
 # Application definition
--- back-end/cart/models.py
+++ back-end/cart/models.py
@@ -3,10 +3,10 @@ from django.db import models
 from items.models import Item, CoreModel
 
 
-class CartItem(models.Model, CoreModel):
+class CartItem(CoreModel):
     quantity = models.TextField(default=1)
     stock_item = models.ForeignKey(Item, on_delete=models.CASCADE)  # Change to StockItem when it's done
 
 
-class FavoriteItem(models.Model, CoreModel):
+class FavoriteItem(CoreModel):
     item = models.ForeignKey(Item, on_delete=models.CASCADE)
