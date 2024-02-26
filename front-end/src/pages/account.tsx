ea562dd4c5377b49d2c35988cb53762d45ba624a
Yehor Korablin <yehorkorablin@gmail.com>
2024-02-26 11:54:30 +0200

    Refactored React project to Next project

--- front-end/src/pages/account.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Account as Verification } from '../components/Account';
-
-export const Account: React.FC = () => (
-  <Verification />
-);
--- front-end/src/pages/product.tsx
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import { Product as Goods } from '../components/Product';
-
-export const Product: React.FC = () => (
-  <Goods />
-);
