6f52765ad932c5eb2ee410043c60836248d94e35
Yehor Korablin <yehorkorablin@gmail.com>
2024-02-29 08:44:24 +0200

    Removed unnecessary imports

--- front-end/src/app/page.tsx
+++ front-end/src/app/page.tsx
@@ -1,7 +1,5 @@
 import Layout from '@/app/layout';
-import { Header } from '@/app/components/Header';
 import { Main } from '@/app/components/Main';
-import { Footer } from '@/app/components/Footer';
 
 export default function HomePage() {
   return (
