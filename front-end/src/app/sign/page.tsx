291114a634ea626398539affe0165ec3ced2b297
Yehor Korablin <yehorkorablin@gmail.com>
2024-03-03 01:16:37 +0200

    Added sign page

---
+++ front-end/src/app/sign/page.tsx
@@ -0,0 +1,10 @@
+import Layout from '@/app/layout';
+import { Sign } from '@/components/Sign';
+
+export default function SignPage() {
+  return (
+    <Layout>
+      <Sign />
+    </Layout>
+  );
+}
