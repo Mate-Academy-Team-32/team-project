148fd81c20c3448b9e839f45616ed8b6aa7b5349
Yehor Korablin <yehorkorablin@gmail.com>
2024-03-03 02:42:31 +0200

    Replaced useless Header`s margin-bottom

--- front-end/src/components/Header/Header.tsx
+++ front-end/src/components/Header/Header.tsx
@@ -21,182 +21,178 @@ export function Header() {
   }, [setHeaderHeight]);
 
   return (
-    <>
-      <header className="Header" style={{ height: headerHight }}>
-        <section className="Header__top-bar">
-          <Image src={iconBurger} alt="burger-menu" className="Header__menu" />
+    <header className="Header" style={{ height: headerHight }}>
+      <section className="Header__top-bar">
+        <Image src={iconBurger} alt="burger-menu" className="Header__menu" />
 
-          <Link href="/" className="Header__logo">
-            <div className="logo">
-              Perfu
-              <span className="logo__highlight">Me</span>
+        <Link href="/" className="Header__logo">
+          <div className="logo">
+            Perfu
+            <span className="logo__highlight">Me</span>
+          </div>
+        </Link>
+
+        <nav className="nav nav--links">
+          <Link href="/" className="nav__link">
+            Home
+          </Link>
+          <Link href="/catalog" className="nav__link" id="catalog">
+            Catalog
+          </Link>
+          <nav className="nav nav--catalog hidden">
+            <Link
+              href="/catalog?filter=top-10"
+              className="nav__link nav__link--upper"
+            >
+              Top 10
+            </Link>
+            <Link
+              href="/catalog?filter=women"
+              className="nav__link nav__link--upper"
+            >
+              Women`s perfumery
+            </Link>
+            <Link
+              href="/catalog?filter=men"
+              className="nav__link nav__link--upper"
+            >
+              Men`s perfumery
+            </Link>
+            <Link
+              href="/catalog?filter=unisex"
+              className="nav__link nav__link--upper"
+            >
+              Unisex
+            </Link>
+          </nav>
+          <Link href="/about" className="nav__link">
+            About Us
+          </Link>
+          <Link href="/contacts" className="nav__link">
+            Contact Us
+          </Link>
+        </nav>
+
+        <nav className="nav nav--sign">
+          <div className="nav__input--search">
+            <button
+              type="button"
+              className="lens lens--close hidden"
+            ></button>
+            <input
+              id="search"
+              type="text"
+              className="nav__input hidden"
+              placeholder="Hey, what are you looking for?"
+            />
+            <button type="button" className="lens lens--open"></button>
+          </div>
+
+          <Link href="/account">
+            <div className={cn('dropdown', isClickedSign && 'is-active')}>
+              <div className="dropdown-trigger">
+                <Image
+                  className="sign-in"
+                  src={iconAccount}
+                  alt="Sign in"
+                  onMouseEnter={() => {
+                    setIsClickedSign((currentState) => !currentState);
+                  }}
+                />
+              </div>
+              {isSigned ? (
+                <div
+                  className="dropdown-menu"
+                  id="dropdown-menu1"
+                  role="menu"
+                >
+                  <div className="dropdown-content">
+                    <div
+                      className="dropdown-item"
+                      onClick={() => {
+                        setIsClickedSign(false);
+                      }}
+                    >
+                      <p>Account details</p>
+                    </div>
+                    <hr className="dropdown-divider" />
+                    <div
+                      className="dropdown-item"
+                      onClick={() => {
+                        setIsClickedSign(false);
+                      }}
+                    >
+                      <p>My orders</p>
+                    </div>
+                    <hr className="dropdown-divider" />
+                    <div
+                      className="dropdown-item"
+                      onClick={() => {
+                        setIsClickedSign(false);
+                      }}
+                    >
+                      <p>Sign out</p>
+                    </div>
+                  </div>
+                </div>
+              ) : (
+                <div
+                  className="dropdown-menu"
+                  id="dropdown-menu1"
+                  role="menu"
+                >
+                  <div className="dropdown-content">
+                    <div
+                      className="dropdown-item"
+                      onClick={() => {
+                        setIsClickedSign(false);
+                      }}
+                    >
+                      <p>
+                        <Link
+                          href="/sign?type=in"
+                          className="dropdown__text dropdown__text--size--16"
+                        >
+                          Sign In
+                        </Link>
+                      </p>
+                    </div>
+                    <hr className="dropdown-divider" />
+                    <div
+                      className="dropdown-item"
+                      onClick={() => {
+                        setIsClickedSign(false);
+                      }}
+                    >
+                      <p className="dropdown__text dropdown__text--color--light">
+                        Don`t have an account?{' '}
+                        <Link
+                          href="/sign?type=up"
+                          className="dropdown__text dropdown__text--bold"
+                        >
+                          Sign Up
+                        </Link>
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              )}
             </div>
           </Link>
 
-          <nav className="nav nav--links">
-            <Link href="/" className="nav__link">
-              Home
-            </Link>
-            <Link href="/catalog" className="nav__link" id="catalog">
-              Catalog
-            </Link>
-            <nav className="nav nav--catalog hidden">
-              <Link
-                href="/catalog?filter=top-10"
-                className="nav__link nav__link--upper"
-              >
-                Top 10
-              </Link>
-              <Link
-                href="/catalog?filter=women"
-                className="nav__link nav__link--upper"
-              >
-                Women`s perfumery
-              </Link>
-              <Link
-                href="/catalog?filter=men"
-                className="nav__link nav__link--upper"
-              >
-                Men`s perfumery
-              </Link>
-              <Link
-                href="/catalog?filter=unisex"
-                className="nav__link nav__link--upper"
-              >
-                Unisex
-              </Link>
-            </nav>
-            <Link href="/about" className="nav__link">
-              About Us
-            </Link>
-            <Link href="/contacts" className="nav__link">
-              Contact Us
-            </Link>
-          </nav>
+          <Link href="/likes" id="icon--like">
+            <Image className="Header__icon" src={iconHeart} alt="Heart" />
+          </Link>
 
-          <nav className="nav nav--sign">
-            <div className="nav__input--search">
-              <button
-                type="button"
-                className="lens lens--close hidden"
-              ></button>
-              <input
-                id="search"
-                type="text"
-                className="nav__input hidden"
-                placeholder="Hey, what are you looking for?"
-              />
-              <button type="button" className="lens lens--open"></button>
-            </div>
-
-            <Link href="/account">
-              <div className={cn('dropdown', isClickedSign && 'is-active')}>
-                <div className="dropdown-trigger">
-                  <Image
-                    className="sign-in"
-                    src={iconAccount}
-                    alt="Sign in"
-                    onMouseEnter={() => {
-                      setIsClickedSign((currentState) => !currentState);
-                    }}
-                  />
-                </div>
-                {isSigned ? (
-                  <div
-                    className="dropdown-menu"
-                    id="dropdown-menu1"
-                    role="menu"
-                  >
-                    <div className="dropdown-content">
-                      <div
-                        className="dropdown-item"
-                        onClick={() => {
-                          setIsClickedSign(false);
-                        }}
-                      >
-                        <p>Account details</p>
-                      </div>
-                      <hr className="dropdown-divider" />
-                      <div
-                        className="dropdown-item"
-                        onClick={() => {
-                          setIsClickedSign(false);
-                        }}
-                      >
-                        <p>My orders</p>
-                      </div>
-                      <hr className="dropdown-divider" />
-                      <div
-                        className="dropdown-item"
-                        onClick={() => {
-                          setIsClickedSign(false);
-                        }}
-                      >
-                        <p>Sign out</p>
-                      </div>
-                    </div>
-                  </div>
-                ) : (
-                  <div
-                    className="dropdown-menu"
-                    id="dropdown-menu1"
-                    role="menu"
-                  >
-                    <div className="dropdown-content">
-                      <div
-                        className="dropdown-item"
-                        onClick={() => {
-                          setIsClickedSign(false);
-                        }}
-                      >
-                        <p>
-                          <Link
-                            href="/sign?type=in"
-                            className="dropdown__text dropdown__text--size--16"
-                          >
-                            Sign In
-                          </Link>
-                        </p>
-                      </div>
-                      <hr className="dropdown-divider" />
-                      <div
-                        className="dropdown-item"
-                        onClick={() => {
-                          setIsClickedSign(false);
-                        }}
-                      >
-                        <p className="dropdown__text dropdown__text--color--light">
-                          Don`t have an account?{' '}
-                          <Link
-                            href="/sign?type=up"
-                            className="dropdown__text dropdown__text--bold"
-                          >
-                            Sign Up
-                          </Link>
-                        </p>
-                      </div>
-                    </div>
-                  </div>
-                )}
-              </div>
-            </Link>
-
-            <Link href="/likes" id="icon--like">
-              <Image className="Header__icon" src={iconHeart} alt="Heart" />
-            </Link>
-
-            <Link href="/cart" id="bag">
-              <Image
-                className="Header__icon Header__icon--w-34"
-                src={iconBag}
-                alt="Bag"
-              />
-            </Link>
-          </nav>
-        </section>
-      </header>
-
-      <div className="margin" style={{ marginTop: headerHight }}></div>
-    </>
+          <Link href="/cart" id="bag">
+            <Image
+              className="Header__icon Header__icon--w-34"
+              src={iconBag}
+              alt="Bag"
+            />
+          </Link>
+        </nav>
+      </section>
+    </header>
   );
 }
