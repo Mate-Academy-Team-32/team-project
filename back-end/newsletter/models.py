dd97e9342c13ec68268772a30d725dae4b196768
Chookity <o.chookity@gmail.com>
2024-03-07 18:18:16 +0200

    Add `html_file` field for `Newsletter` model

--- back-end/newsletter/models.py
+++ back-end/newsletter/models.py
@@ -1,6 +1,17 @@
+import os
+import uuid
+from django.utils.text import slugify
 from django.db import models
 
 from items.models import CoreModel
+
+
+def get_file_path(instance, filename):
+    _, extension = os.path.splitext(filename)
+    filename = f"{slugify(str(instance))}-{uuid.uuid4()}{extension}"
+    dirname = f"{slugify(type(instance).__name__)}s/"
+
+    return os.path.join("templates/", dirname, filename)
 
 
 class Newsletter(CoreModel):
@@ -8,6 +19,7 @@ class Newsletter(CoreModel):
 
     subject = models.CharField(max_length=255, default=DEFAULT_SUBJECT)
     text_content = models.TextField()
+    html_file = models.FileField(blank=True, null=True, upload_to=get_file_path)
     scheduled_time = models.DateTimeField(blank=True, null=True)
 
     def __str__(self):
