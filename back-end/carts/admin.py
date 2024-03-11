e1f07cbd9c5a113492adc2d318178f4f34ec700f
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2024-03-10 23:48:44 -0500

    Register cart and favorite models to admin, fix users model in admin

--- back-end/carts/admin.py
+++ back-end/carts/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+from carts.models import CartItem, FavoriteItem
+from items.admin import CoreModelAdmin
+
+
+@admin.register(CartItem)
+class CartItemAdmin(CoreModelAdmin):
+    search_fields = ("created_by__email",) + CoreModelAdmin.search_fields
+    ordering = ("created_by__email", "-created_at")
+    list_display = (
+        "created_by",
+        "stock_item",
+        "quantity",
+    ) + CoreModelAdmin.list_display
+
+
+@admin.register(FavoriteItem)
+class FavoriteAdmin(CoreModelAdmin):
+    search_fields = ("created_by__email", "item",) + CoreModelAdmin.search_fields
+    ordering = ("created_by__email", "-created_at")
+    list_display = (
+        "item",
+    ) + CoreModelAdmin.list_display
