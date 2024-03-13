ad6240eabfc2ace44be186e021e7208ec452cfb7
Stanislav Nikulytsia <nikulicastas2004@gmail.com>
2024-03-13 11:39:04 -0500

    Add webhook for payments, add order fulfill logic

--- back-end/orders/urls.py
+++ back-end/orders/urls.py
@@ -1,11 +1,14 @@
+from django.urls import path
 from rest_framework import routers
 
-from orders.views import OrderViewSet, PaymentViewSet
+from orders.views import OrderViewSet, PaymentViewSet, stripe_webhook
 
 router = routers.DefaultRouter()
 router.register("orders", OrderViewSet)
 router.register(r"payments", PaymentViewSet, basename="payment-detail")
 
-urlpatterns = router.urls
+urlpatterns = [
+    path("webhooks/stripe", stripe_webhook, name="stripe-webhook"),
+] + router.urls
 
 app_name = "orders"
--- back-end/orders/views.py
+++ back-end/orders/views.py
@@ -1,6 +1,10 @@
+import stripe
+from django.conf import settings
+
+from django.http import HttpResponse
 from django.db import transaction
 from django.shortcuts import get_object_or_404
-from django.views import View
+from django.views.decorators.csrf import csrf_exempt
 from rest_framework import mixins, viewsets, status
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
@@ -10,7 +14,7 @@ from carts.models import CartItem
 from items.views import CoreModelMixin
 from orders.models import Order, OrderItem, Payment
 from orders.serializers import OrderSerializer, PaymentSerializer
-from orders.utils import create_new_payment
+from orders.utils import create_new_payment, fulfill_order
 
 
 class OrderViewSet(
@@ -64,6 +68,7 @@ class OrderViewSet(
         serializer = self.get_serializer(new_order)
         return Response(serializer.data, status=status.HTTP_201_CREATED)
 
+    @transaction.atomic
     def create(self, request, *args, **kwargs):
         response = super().create(request, *args, **kwargs)
         pk = response.data.get("id")
@@ -86,3 +91,37 @@ class PaymentViewSet(
         return Payment.objects.filter(order__created_by=self.request.user)
 
     serializer_class = PaymentSerializer
+
+
+stripe.api_key = settings.STRIPE_SECRET_KEY
+
+
+@csrf_exempt
+def stripe_webhook(request):
+    payload = request.body
+    sig_header = request.META['HTTP_STRIPE_SIGNATURE']
+    event = None
+
+    try:
+        event = stripe.Webhook.construct_event(
+            payload, sig_header, settings.STRIPE_WEBHOOK_SECRET
+        )
+    except ValueError as e:
+        # Invalid payload
+        return HttpResponse(status=400)
+    except stripe.error.SignatureVerificationError as e:
+        # Invalid signature
+        return HttpResponse(status=400)
+
+    if event['type'] == 'checkout.session.completed':
+        # Retrieve the session. If you require line items in the response, you may include them by expanding line_items.
+        session = stripe.checkout.Session.retrieve(
+            event['data']['object']['id'],
+            expand=['line_items'],
+        )
+
+        # Fulfill the purchase...
+        fulfill_order(session)
+
+    # Passed signature verification
+    return HttpResponse(status=200)
