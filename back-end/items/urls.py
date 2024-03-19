8d2d79733684a7feec89c8070f1ccaea2f99ec40
Chookity <o.chookity@gmail.com>
2024-03-19 19:40:20 +0200

    Add endpoint for `NoteViewSet`

--- back-end/items/urls.py
+++ back-end/items/urls.py
@@ -7,12 +7,14 @@ from items.views import (
     ReviewViewSet,
     BrandViewSet,
     StockItemViewSet,
+    NoteViewSet,
 )
 
 router = routers.DefaultRouter()
 router.register("images", ItemImageViewSet)
 router.register("brands", BrandViewSet)
 router.register("tags", TagViewSet)
+router.register("notes", NoteViewSet)
 router.register("reviews", ReviewViewSet)
 router.register("stock_items", StockItemViewSet)
 router.register("", ItemViewSet)
