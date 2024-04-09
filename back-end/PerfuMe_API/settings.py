bda993925d774ec96964408d01bda18db617bca8
stanislavnikulytsia <nikulicastas2004@gmail.com>
2024-04-09 13:10:08 -0500

    Minor fixes

--- back-end/PerfuMe_API/settings.py
+++ back-end/PerfuMe_API/settings.py
@@ -29,7 +29,7 @@ DEBUG = bool(os.environ.get("DJANGO_DEBUG", default=0))
 
 ALLOWED_HOSTS = os.environ.get("DJANGO_ALLOWED_HOSTS").split(" ")
 CORS_ALLOWED_ORIGINS = os.environ.get("CORS_ALLOWED_ORIGINS").split(" ")
-CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS")
+CSRF_TRUSTED_ORIGINS = os.environ.get("CSRF_TRUSTED_ORIGINS").split(" ")
 
 
 # Application definition
